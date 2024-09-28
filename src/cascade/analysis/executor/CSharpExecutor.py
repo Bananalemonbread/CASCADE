92555de272f6f758c674f3fd0dad660539a51f79
Martin Reuter <>
2024-09-28 15:35:37 +0200

    Added CSharpExecutor.py, added docker image reference, still needs implementation

---
+++ src/cascade/analysis/executor/CSharpExecutor.py
@@ -0,0 +1,4 @@
+#TODO: implement this
+
+# https://hub.docker.com/r/microsoft/dotnet-sdk/
+# Use the latest sdk version, as they are backwards compatible --> .NET 9.0 SDK can build .NET 6.0 projects
