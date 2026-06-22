dbf1858ca57c83d0cde8c694feeb31e04cf3c0a0
Bananalemonbread <Bubzbubz12@gmail.com>
2026-06-22 10:48:17 +0200

    removing unnecessary files

--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -1,9 +1,19 @@
-/// Returns the sum of `a` and `b`.
+/// Returns the sum of a + b.
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-/// Returns `a`.
-pub fn return_a(a: i32) -> i32 {
-    29
+/// Returns the difference between a - b.
+pub fn sub(a: i32, b:i32) -> i32 {
+    b - a
+}
+
+///
+pub fn dummy1() {
+    0
+}
+
+/// dummy method that does nothing and has no code
+pub fn dummy2() {
+    
 }
